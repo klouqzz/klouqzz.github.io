c354aaf5410f15ca3227197dd279b8c59b8cf8ca
klouqz <65308344+klouqzz@users.noreply.github.com>
2022-10-11 20:09:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["Web Designer", "Developer", "Student", "Designer", "Freelancer"],
+    strings: ["Web Designer", "Developer", "Student", "Designer", "Freelancer", "Financial Literate", "Android Developer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
   });
 
   var typed = new Typed(".typing-2", {
-    strings: ["Web Designer", "Developer", "Student", "Designer", "Freelancer"],
+    strings: ["Web Designer", "Developer", "Student", "Designer", "Freelancer", "Financial Literate", "Android Developer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
